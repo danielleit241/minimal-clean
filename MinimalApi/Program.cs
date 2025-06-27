638628523062df54a095c89ae5fef44aff059a49
danielleit241 <danielleee241@gmail.com>
2025-06-27 11:44:48 +0700

    Add EF Core and MediatR; update project structure
    - Updated `Application.csproj` to include MediatR v12.5.0.
    - Updated `MinimalApi.csproj` with EF Core packages (v8.0.17) and added project reference to `DataAccess.csproj`.
    - Modified `Program.cs` to configure `SocialDbContext` and register `IPostRepository`.
    - Restructured `launchSettings.json` for new profile settings.
    - Updated `appsettings.json` with a new database connection string.
    - Created migration file `20250627043713_Initial.cs` for `Posts` table schema.
    - Generated designer and snapshot files for migration support.

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,16 @@
+using Application.Abstractions;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SocialDbContext>(
+    opt => opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 
 var app = builder.Build();
 
